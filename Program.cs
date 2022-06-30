bb03b10ef1d311e0480b40e52f5441e79edb008f
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-06-30 19:32:17 -0300

    add com cor na letra

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Writeline ("Mulheres, mulheres, joias raras" foreground color = ConsoleColor.Green);
+Console.Writeline ("Carros rápidos, carros rápidos, muitas gatas, yo" foreground color = ConsoleColor.Green);
+Console.Writeline ("Minha cama tá lotada, tô cheio de grana, yo" foreground color = ConsoleColor.Green);
+Console.Writeline ("Tô cheio de grana, cara, cara" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Meu beck) Compro drogas (Meu beck) Uso elas" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Tô fumando) Muito foda (Não me enche) Me bate a brisa" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Meu beck) Levanto voo (Meu beck) Caralho, tô alto" foreground color = ConsoleColor.Green);
+Console.Writeline ("Caralho, tô alto (Tô fumando, não me enche)" foreground color = ConsoleColor.Green);
+Console.Writeline ("Pico Lanterna Verde, mas o verde é da maconha" foreground color = ConsoleColor.Green);
+Console.Writeline ("Moleque, descabelei na bronha, vou fumar muita maconha, yo" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Caraio) 'Cê não pode me impedir (Caraio) 'Cê não vai me impedir" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Caraio) Não dá pra me impedir, pode tentar me impedir" foreground color = ConsoleColor.Green);
+Console.Writeline ("Mas aí, não dá (Não dá)" foreground color = ConsoleColor.Green);
+
+Console.Readline ("");
+
+Console.Writeline ("Mulheres, mulheres, joias raras" foreground color = ConsoleColor.Green);
+Console.Writeline ("Carros rápidos, carros rápidos, muitas gatas, yo" foreground color = ConsoleColor.Green);
+Console.Writeline ("Minha cama tá lotada, tô cheio de grana, yo" foreground color = ConsoleColor.Green);
+Console.Writeline ("Tô cheio de grana, cara, cara" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Meu beck) Compro drogas (Meu beck) Uso elas" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Tô fumando) Muito foda (Não me enche) Me bate a brisa" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Meu beck) Levanto voo (Meu beck) Caralho, tô alto" foreground color = ConsoleColor.Green);
+Console.Writeline ("Caralho, tô alto (Tô fumando, não me enche)" foreground color = ConsoleColor.Green);
+Console.Writeline ("Pico Lanterna Verde, mas o verde é da maconha" foreground color = ConsoleColor.Green);
+Console.Writeline ("Moleque, descabelei na bronha, vou fumar muita maconha, yo" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Caraio) 'Cê não pode me impedir (Caraio) 'Cê não vai me impedir" foreground color = ConsoleColor.Green);
+Console.Writeline ("(Caraio) Não dá pra me impedir, pode tentar me impedir" foreground color = ConsoleColor.Green);
+Console.Writeline ("Mas aí, não dá (Não dá)" foreground color = ConsoleColor.Green);
